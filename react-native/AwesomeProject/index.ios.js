6c1240dd8197a14bf13eb45e7a1f33ff68b151cd
Marius Küng <maku@mailbox.org>
2015-12-01 10:19:58 +0100

    [RN] Fetching real data

--- react-native/AwesomeProject/index.ios.js
+++ react-native/AwesomeProject/index.ios.js
@@ -14,6 +14,13 @@ var MOCKED_MOVIES_DATA = [
 ];
 
 var React = require('react-native');
+
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var {
   AppRegistry,
   Image,
@@ -23,8 +30,48 @@ var {
 } = React;
 
 var AwesomeProject = React.createClass({
+
+  getInitialState: function() {
+    return {
+      movies: null,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
+
   render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  },
+
+  renderMovie: function(movie) {
     return (
       <View style={styles.container}>
         <Image
@@ -37,7 +84,7 @@ var AwesomeProject = React.createClass({
         </View>
       </View>
     );
-  }
+  },
 });
 
 var styles = StyleSheet.create({
