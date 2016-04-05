96efd6eb233bd24e9e01806431406017e074a315
Marius Küng <maku@mailbox.org>
2016-04-05 09:28:36 +0200

    Added App, Header, Order & Inventory components

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,16 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const StorePicker = React.createClass({
-  render: () => {
+/*
+  App
+  <App/>
+*/
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header/>
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
+    );
+  }
+}
+
+/*
+  Header
+  <Header/>
+*/
+
+class Header extends React.Component {
+  render() {
+    return (
+      <p>Header</p>
+    )
+  }
+}
+
+/*
+  Order
+  <Order/>
+*/
+
+class Order extends React.Component {
+  render() {
+    return (
+      <p>Order</p>
+    )
+  }
+}
+
+/*
+  Inventory
+  <Inventory/>
+*/
+
+class Inventory extends React.Component {
+  render() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+}
+
+/*
+  StorePicker
+  This will let us make <StorePicker/>
+*/
+
+class StorePicker extends React.Component {
+  render() {
     return (
       <form className="store-selector">
         <h2>Please Enter A Store</h2>
         <input type="text" ref="storeId"/>
         <input type="submit"/>
       </form>
-    );
+    )
   }
-});
+}
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
