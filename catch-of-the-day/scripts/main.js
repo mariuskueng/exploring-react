28bcad6fbaa87f15b33365f6c03cce6066a40e60
Marius Küng <maku@mailbox.org>
2016-04-05 09:10:11 +0200

    Added StorePicker component

---
+++ catch-of-the-day/scripts/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const StorePicker = React.createClass({
+  render: () => {
+    return (
+      <p>hello</p>
+    );
+  }
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
