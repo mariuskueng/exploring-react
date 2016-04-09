c792f7f157d44e4a101334e298919540fe338544
Marius Küng <maku@mailbox.org>
2016-04-09 11:55:17 +0200

    Added propTypes validations to Order and Inventory components

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -181,6 +181,9 @@ var Header = React.createClass({
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
+  },
+  propTypes : {
+    tagline : React.PropTypes.string.isRequired
   }
 })
 
@@ -245,6 +248,11 @@ var Order = React.createClass({
 
       </div>
     )
+  },
+  propTypes : {
+    fishes: React.PropTypes.object.isRequired,
+    order: React.PropTypes.object.isRequired,
+    removeFromOrder: React.PropTypes.func.isRequired
   }
 })
 
@@ -282,6 +290,13 @@ var Inventory = React.createClass({
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
+  },
+  propTypes: {
+    addFish : React.PropTypes.func.isRequired,
+    loadSamples : React.PropTypes.func.isRequired,
+    fishes : React.PropTypes.object.isRequired,
+    linkState : React.PropTypes.func.isRequired,
+    removeFish : React.PropTypes.func.isRequired,
   }
 })
 
