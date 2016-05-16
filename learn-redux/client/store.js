8790f0955c56835ce44f5daaeb31cbc48f2a8b56
Marius Küng <maku@mailbox.org>
2016-05-16 18:59:37 +0200

    Added reducers

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -4,7 +4,6 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
 // import the root reducer
-
 import rootReducer from './reducers/index';
 
 import comments from './data/comments';
